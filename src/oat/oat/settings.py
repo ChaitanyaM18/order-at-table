b455a780e94078bb82baead95c7329fcb1f8bd65
chaitanya <chaitanyamaralihalli18@gmail.com>
2021-10-10 02:31:41 +0530

    fixes

--- src/oat/oat/settings.py
+++ src/oat/oat/settings.py
@@ -17,8 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '*io)&5ozg**$5os*%3!#nxud-=ab%qj(7%ed*g26g2vj9^fvnu'
